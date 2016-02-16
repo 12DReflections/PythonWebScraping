3dea0c1c04fcc5355636dee5873fa11c274aece8
Julian Wise - WLAPTOP <Julianadamwise@gmail.com>
2016-02-16 18:13:37 +1100

    Multi threaded scrape

---
+++ 7_MultiThread/multiThread.py
@@ -0,0 +1,42 @@
+from threading import Thread
+import urllib2
+import re
+import os
+import MySQLdb
+
+
+# A multithreaded share pull
+  
+# Chage working directory
+direct = "C:\PythonPrograms\\7_MultiThread\\"
+os.chdir(direct)
+
+# Stringify words
+symbolsList = open("ASX_Top100.txt").read()  
+symbolsList = symbolsList.split("\n")
+
+print symbolsList
+
+def thread(shareCode):
+    pageUrl = "https://au.finance.yahoo.com/q?s=" + shareCode + ".AX"
+    htmlfile = urllib2.urlopen(pageUrl)
+    htmltext = htmlfile.read()
+
+    regex = '<span id="yfs_l84_'+shareCode.lower()+'.ax">(.+?)</span>'
+    pattern = re.compile(regex)
+    
+    results = re.findall(pattern,htmltext)
+    print "the price of " + str(shareCode) + " is " + str(results[0])
+    
+
+threadList = []
+
+for symbol in symbolsList:
+    print symbol
+    target = Thread(target=thread,args=(symbol,))
+    target.start()
+    threadList.append(target)
+    
+#Join threads back
+for b in threadList:
+    b.join()
