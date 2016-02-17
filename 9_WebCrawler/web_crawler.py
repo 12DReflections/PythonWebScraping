1deaa44bfa1c30955a83d331d0d86bcc0f9eef96
JulianW <julianadamwise@gmail.com>
2016-02-17 17:57:50 +1100

    Web Crawler queue implementation

--- 9_WebCrawler/web_crawler.py
+++ 9_WebCrawler/web_crawler.py
@@ -3,9 +3,37 @@ from bs4 import BeautifulSoup
 import urlparse
 import os
 
-#direct = "C:\PythonPrograms\PythonWebScraping\9_WebCrawler\web_crawler.py"
+# Spider Web Crawler Algorithm
+# Create que of URLs, visit new URLs, Keep track of URLs
+# Retrieve information from Website,
+#   use for new Websites with new information 
+
+#direct = "C:\PythonPrograms\PythonWebScraping\9_WebCrawler\"
 direct = "C:\PythonPrograms\PythonWebScraping\9_WebCrawler"
 
 os.chdir(direct)
 
-htmltext = urllib2.urlopen("http://www.google.com")
+url = "http://www.mileycyrus.com"
+
+
+urlList = [url]     #Stack of URLs
+visitedList = [url] #List of visited URLs
+
+# Add to end of urlList, pop from start
+while len(urlList) > 0:
+    try:
+        htmltext = urllib2.urlopen(urlList[0]).read() # que
+    except:
+        print "Error ", urlList[0]
+
+    soup = BeautifulSoup(htmltext, "html.parser")
+    urlList.pop(0) #pop off first element of array
+    print 'Length of urlList: ', len(urlList)
+    
+    for tag in soup.findAll('a', href = True):
+        tag['href'] = urlparse.urljoin(url, tag['href']) #append top level domain if doesn't exist
+        if url in tag['href'] and tag['href'] not in visitedList:
+            urlList.append(tag['href'])     #temp jobs to be processed
+            visitedList.append(tag['href']) #list of visited URLs
+
+print visitedList                             
