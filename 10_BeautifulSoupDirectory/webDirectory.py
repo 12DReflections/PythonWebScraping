7561409f1acd17615b21593798538859a6a70c21
JulianW <julianadamwise@gmail.com>
2016-02-18 13:58:07 +1100

    Search through all site directories

---
+++ 10_BeautifulSoupDirectory/webDirectory.py
@@ -0,0 +1,19 @@
+import urllib2
+from bs4 import BeautifulSoup
+import urlparse
+
+# Print all web URL's and all directories of site
+
+def main():
+	url = "http://cityhealthandmassage.com.au"
+	base = "http://cityhealthandmassage.com.au/"
+
+	htmlfile = urllib2.urlopen(url)
+	soup = BeautifulSoup(htmlfile, "html.parser")
+
+	for tag in soup.findAll('a', href = True):
+		print base + tag['href']
+
+
+if __name__ == "__main__":
+    main()
