16302a6b8e8ca681105848bee2ab5bb50e37db9a
Julian Wise - WLAPTOP <Julianadamwise@gmail.com>
2016-08-24 21:09:45 +1000

    tidy

--- 17_MongoInterface/mongoget.py
+++ 17_MongoInterface/mongoget.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from bson import json_util
 
 #Connect to Mongo Client 
-client = MongoClient('uri')
+client = MongoClient('user://pword:dreamerthewise@ds047124.mlab.com:47124/politicians_from_theage')
 db = client.politicians_from_theage #define database used
 print db
 # Define Collection
@@ -20,16 +20,11 @@ for doc in cursor:
     json_doc = json.dumps(doc, default=json_util.default)
     json_docs.append(json_doc)
 
-print json_docs[1]
-# for a in cursor:
-#  	print a
 
-print 'kitt'
-'''
-# Define Cursor
-my_cursor = collection.find()
-print my_cursor # prints<pymongo.cursor.Cursor object at 0x0000000003247518>
-'''
+
+
+print 'kitty'
+
 
 
 
