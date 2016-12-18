81f23a9d979d7847e06c210ed54cab2254f6e941
Julian Wise - D <Julianadamwise@gmail.com>
2016-12-18 13:30:41 +1100

    Fix output

--- 18_ParseNonUTF/parsenonutf.py
+++ 18_ParseNonUTF/parsenonutf.py
@@ -4,11 +4,18 @@ import string
 Clean non utf characters from string
 '''
 
-## Method 1
+
+def cleanstring(invalidString):
+	## Method 1
+	printable = set(string.printable)
+	cleanstring = filter(lambda x: x in printable, invalidString)
+	print cleanstring
+
 invalidString = 'this is a string \xe2\x82\xa1 \xf0\x28\x8c\x28 \xfc\xa1\xa1\xa1\xa1\xa1'
-printable = set(string.printable)
-cleanstring = filter(lambda x: x in printable, invalidString)
-print cleanstring
+	
+cleanstring(invalidString)
+
+
 #http://stackoverflow.com/questions/8689795/how-can-i-remove-non-ascii-characters-but-leave-periods-and-spaces-using-python
 
 ## Method 2
--- 23_Page_Keywords/main.py
+++ 23_Page_Keywords/main.py
@@ -1,95 +1,94 @@
 
 from bs4 import BeautifulSoup
-
 from urllib2 import urlopen
-
-
-
-
+import string
 
 def main():
 	
 	mylist = clean_list()
+	
+	#mylist = ['http://www.cairnsindoorsports.com.au', 'http://www.poleplaystudios.com.au', 'http://www.jennycraig.com.au']
+
 	#print mylist
 
 	website_list = mylist
 	
 	
 	meta_data = title_description_keywords(website_list)
-	print meta_data
-
-
+	#print meta_data
+	# #Clean non utf chars
+	#print meta_data
+	#print 'blue'
+	# clean_meta_data = clean_non_utf(meta_data)
+ 
+	# print clean_meta_data
+	# print 'orange'
 	# Separate on tab
-	data_string = "".join(str(x) + '\t' for x in meta_data)
-	print data_string
 
-	column = 0
+	
 
-	for char in data_string:
-		if char == '\t':
-			column+=1
-			if column == 4:
-				char = '\n'
-				column = 0
+	with open("Output6.txt", "w") as text_file:
+	    for x in meta_data:
+			x = x.encode('ascii',errors='ignore')
+			text_file.write("%s0110" % x)
 
-	print data_string
+	text_file.close()
+
+
+	# print data_string
 # Inputs a list of websites and outputs their site_title_desc_key as a list 
 def title_description_keywords(website_list):
 	site_title_desc_key = []
 
 	for website in website_list:
 	# Open web page into a string
-		try:
-			html = urlopen(website).read()
-			#print html success!
-			soup = BeautifulSoup(html, "lxml")
-		except: 
-			continue
+		print website
+		html = urlopen(website).read()
+		#print html success!
+		soup = BeautifulSoup(html, "lxml")
+
+		# Append the website
+		site_title_desc_key.append(website)
+
 		try:
 			# Get the title
-			title = soup.title.string
+			title = soup.title.string  + '\t'
 		except: 
-			title = " "
+			title = " x " + '\t'
 			continue
-
+		site_title_desc_key.append(title)
+		
 		try: 	
 			# Get the description
 			html_desc = soup.findAll(attrs={"name":"description"}) 
-			description = html_desc[0]['content'].encode('utf-8')
+			description = html_desc[0]['content'].encode('utf-8')  + '\t'
+			
 		except:
-			description = " "
+			description = " x "  + '\t'
 			continue
+		site_title_desc_key.append(description)
+
 		try:
 			# Get the keywords
 			html_keyword = soup.findAll(attrs={"name":"keywords"}) 
-			keyword = html_keyword[0]['content'].encode('utf-8')
+			keyword = html_keyword[0]['content'].encode('utf-8')  + '\t'
 		except:
-			keywords = " "
+			keywords = " x "  + '\t'
 			continue		
+		site_title_desc_key.append(keyword)
+		site_title_desc_key.append('\n')
+
 		# print title
 		# print description
 		# print keyword
-
-		site_title_desc_key.append(website)
-		site_title_desc_key.append(title)
-		site_title_desc_key.append(description)
-		site_title_desc_key.append(keyword + '\n')
 	return site_title_desc_key
 
 
-# Get a File of Strings into a deduplicated list
-def clean_list():
-	clean_list = []		 	
-	website_file = open("company_list.txt")
-	symbols_string = website_file.read()
-	
-	for line in symbols_string.splitlines():
-		if line not in clean_list:
-			clean_list.append(line)
 
-	return clean_list
-
-
+def clean_non_utf(invalidString):
+	for item in invalidString:
+		cleanstring = item.encode('ascii',errors='ignore')
+	return cleanstring
 
 
 if __name__=='__main__':
