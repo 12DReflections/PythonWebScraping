5e726abe2e524bbb6714a76f49f6472a90292744
Julian <julianadamwise@gmail.com>
2017-01-22 10:46:03 +1100

    public key push

--- 5_MinuteStock_NASDAQ/minuteStockMechanize.py
+++ 5_MinuteStock_NASDAQ/minuteStockMechanize.py
@@ -7,6 +7,7 @@ from mechanize import Browser
 # Use mechanize to hide as a browser and view the stock updates
 # Websites cannot see this as a bot
 
+
 def main():
 	ua = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0 (compatible;)'
 	mechBrowser = Browser()
